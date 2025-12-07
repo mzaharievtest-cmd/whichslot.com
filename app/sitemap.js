dd71d83a4d4fb1ec695600e50910c8c3f7492d92
mzaharievtest-cmd <mzaharievtest@gmail.com>
2025-12-07 19:00:40 +0100

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://whichslot.com/", lastModified: new Date() },
+    { url: "https://whichslot.com/wheel", lastModified: new Date() },
+    { url: "https://whichslot.com/slots", lastModified: new Date() },
+    { url: "https://whichslot.com/casinos", lastModified: new Date() }
+  ];
+}
